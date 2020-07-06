c97f24887591fe10402209a7df886767d12cae45
arun kumar <alekhya0801@gmail.com>
2020-07-06 23:19:01 +0530

    Added : Quantity Measurement Rest API Documentation using Swagger2

--- src/main/java/com/quantitymeasurement/controller/QuantityMeasurementController.java
+++ src/main/java/com/quantitymeasurement/controller/QuantityMeasurementController.java
@@ -4,6 +4,8 @@ import com.quantitymeasurement.enumeration.Unit;
 import com.quantitymeasurement.exception.QuantityMeasurementException;
 import com.quantitymeasurement.model.QuantityMeasurement;
 import com.quantitymeasurement.service.QuantityMeasurementService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ public class QuantityMeasurementController {
     @Autowired
     QuantityMeasurementService quantityMeasurementService;
 
+    @ApiResponses(value = {@ApiResponse(code = 400,message = "Bad Request")})
+
     @GetMapping("/quantity-measurement/{unitIn}/{value}/{unitOut}")
     public QuantityMeasurement getConvertedValue(@PathVariable("unitIn") String unitIn,@PathVariable("value") Double value,
                                                  @PathVariable("unitOut") String unitOut) {
@@ -22,11 +26,11 @@ public class QuantityMeasurementController {
             return new QuantityMeasurement(quantityMeasurementService.convertTo(new QuantityMeasurement(
                                                                                 value,Unit.valueOf(unitIn.toUpperCase())),
                                                                                 Unit.valueOf(unitOut.toUpperCase())),
-                                            Unit.valueOf(unitOut.toUpperCase()));
+                                           Unit.valueOf(unitOut.toUpperCase()));
         } catch (IllegalArgumentException e) {
             throw new QuantityMeasurementException("Invalid Unit");
         } catch (QuantityMeasurementException e) {
-            throw new QuantityMeasurementException(unitOut + " cann't convert to "+ unitIn);
+            throw new QuantityMeasurementException(unitOut.toUpperCase() + " cann't convert to "+ unitIn.toUpperCase());
         }
     }
 
