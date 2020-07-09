61976c65bc6bb3df7b49d5dda61b9fe516040cf4
arun kumar <alekhya0801@gmail.com>
2020-07-09 14:33:15 +0530

    Added : handle default exception

--- src/main/java/com/quantitymeasurement/exception/QuantityMeasurementExceptionHandler.java
+++ src/main/java/com/quantitymeasurement/exception/QuantityMeasurementExceptionHandler.java
@@ -3,28 +3,34 @@ package com.quantitymeasurement.exception;
 import com.quantitymeasurement.enumeration.Message;
 import com.quantitymeasurement.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import static com.quantitymeasurement.enumeration.Message.INVALID_INPUT;
+import static com.quantitymeasurement.enumeration.Message.TRY_AGAIN_LATER;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class QuantityMeasurementExceptionHandler {
 
     @Autowired
     Response response;
 
-    @ExceptionHandler
+    @ExceptionHandler(QuantityMeasurementException.class)
     public ResponseEntity<Response> addException(QuantityMeasurementException e){
         return getResponseEntity(e.exceptionType,e.status);
     }
 
-    @ExceptionHandler(ConversionFailedException.class)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Response> addException(){
         return getResponseEntity(INVALID_INPUT,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> addCustomException(){
+        return getResponseEntity(TRY_AGAIN_LATER,HttpStatus.NOT_FOUND);
     }
 
     public ResponseEntity<Response> getResponseEntity(Message message,HttpStatus status){
